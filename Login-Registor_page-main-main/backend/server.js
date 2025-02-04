3cf0a7a3acc51f0a68f550f4202d5e7b5a1bc9ff
Gajendra Gour <gajendragour8747@gmail.com>
2025-02-04 11:36:15 +0530

    done

--- Login-Registor_page-main-main/backend/server.js
+++ Login-Registor_page-main-main/backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // Enable CORS for your frontend URL
 app.use(
   cors({
-    origin: "", // Update to your frontend URL
+    origin: "https://login-registor-page-main-main-bcvv.vercel.app", // Update to your frontend URL
     methods: ["GET","POST","PUT","DELETE"],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true // Allow cookies and credentials if needed
